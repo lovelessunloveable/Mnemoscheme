f604ffca1676bf9953e99b176883ec7535cfaab4
murphyqwek <qwekinaft@list.ru>
2024-03-11 17:25:55 +0700

    Add Abstract Port class, Ports interfaces and UniversalPort class

---
+++ Mnemoscheme/Models/Ports/IPort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mnemoscheme.Models.Ports
+{
+    abstract class IPort
+    {
+        protected SerialPort _serialPort;
+        public string PortName => _serialPort.PortName;
+        public int BaudRate => _serialPort.BaudRate;
+
+        public IPort()
+        {
+            _serialPort = new SerialPort();
+        }
+
+        public IPort(string PortName)
+        {
+            _serialPort = new SerialPort();
+
+            SetPortName(PortName);
+        }
+
+        public IPort(string PortName, int BaudRate)
+        {
+            _serialPort = new SerialPort();
+
+            SetPortName(PortName);
+            SetBaudRate(BaudRate);
+        }
+
+        public virtual void SetPortName(string PortName)
+        {
+            _serialPort.PortName = PortName;
+        }
+
+        public virtual void SetBaudRate(int BaudRate)
+        {
+            _serialPort.BaudRate = BaudRate;
+        }
+
+        public virtual void Open()
+        {
+            _serialPort.Open();
+        }
+
+        public virtual void Close()
+        {
+            _serialPort.Close();
+        }
+
+    }
+}
---
+++ Mnemoscheme/Models/Ports/UniversalPort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mnemoscheme.Models.Ports
+{
+    internal class UniversalPort : IPort, IPortWritable, IPortReadable
+    {
+        public override void Close()
+        {
+            base.Close();
+
+            //TODO: Close Thread
+        }
+
+        public string ReadLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteLine(string Line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
