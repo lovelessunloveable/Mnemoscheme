f2ba6c92a579d19cbc7df1812b677cbcc7f1df99
lovelessunloveable <145833189+lovelessunloveable@users.noreply.github.com>
2024-03-11 18:27:08 +0700

    Added maximize buttons, added fonts, colours, buttons styles etc.

--- Mnemoscheme/MainWindow.xaml.cs
+++ Mnemoscheme/MainWindow.xaml.cs
@@ -1,28 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WinInterop = System.Windows.Interop;
+using Mnemoscheme.Models.WindowHelper;
 
 namespace Mnemoscheme
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MaximizeHelper maximizeHelper;
+
         public MainWindow()
         {
             InitializeComponent();
+            maximizeHelper = new MaximizeHelper((int)MinHeight, (int)MinWidth);
+            this.SourceInitialized += new EventHandler(MainWindowSourceInitialized);
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeWindow(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState = WindowState.Maximized;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+
+        void MainWindowSourceInitialized(object sender, EventArgs e)
+        {
+            System.IntPtr handle = (new WinInterop.WindowInteropHelper(this)).Handle;
+            WinInterop.HwndSource.FromHwnd(handle).AddHook(new WinInterop.HwndSourceHook(maximizeHelper.WindowProc));
         }
     }
 }
