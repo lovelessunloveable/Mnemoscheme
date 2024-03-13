bd953c8eceae9786414b30c91bd4fa18f87f3ac3
murphyqwek <qwekinaft@list.ru>
2024-03-13 15:37:35 +0700

    Add Logger Class

---
+++ Mnemoscheme/Models/Logs/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mnemoscheme.Models.Logs
+{
+    public enum LogType
+    {
+        INFO,
+        WARNING,
+        ERROR,
+    }
+
+    internal class Logger
+    {
+    }
+}
