126b9aa440beeb3a3973577aba4c3c59ba56faba
murphyqwek <qwekinaft@list.ru>
2024-03-19 02:10:58 +0700

    Add Reactor Devices

---
+++ Mnemoscheme/Models/Devices/Reactor/ReactorDeviceState.cs
@@ -0,0 +1,21 @@
+﻿using Mnemoscheme.Models.Devices.Reactor.SynthesConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mnemoscheme.Models.Devices.Reactor
+{
+    internal class ReactorDeviceState
+    {
+
+
+
+
+
+        private int _experimentTime;
+        private Preset _preset;
+        private TokMode _tokMode;
+    }
+}
