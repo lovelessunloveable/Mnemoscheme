12497d50ac9981ca408df0b6452e02ed930295fd
lovelessunloveable <145833189+lovelessunloveable@users.noreply.github.com>
2024-03-18 23:13:30 +0700

    added borders, and logger.cs

---
+++ Mnemoscheme/Models/Logger/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mnemoscheme.Models.Logger
+{
+    internal class Logger
+    {
+        void SaveInfo(string message) {
+        }
+        void SaveError(string message) { 
+        }
+
+        static void ExportLogger(string message) { 
+        }
+    }
+}
